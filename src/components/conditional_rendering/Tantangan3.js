5e1b0e85a110b7599ff2f0a72194f1287207bafe
m.romdoni101102 <m.romdoni101102@gmail.com>
2025-07-20 12:44:06 +0700

    fixed code

--- src/components/conditional_rendering/Tantangan3.js
+++ src/components/conditional_rendering/Tantangan3.js
@@ -6,7 +6,7 @@ function Drink({ name }) {
         <dt>Part of plant</dt>
         <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
         <dt>Caffeine content</dt>
-        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+        <dd>{name === 'tea' ? '15-70 mg/cup' : '80-185 mg/cup'}</dd>
         <dt>Age</dt>
         <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
       </dl>
